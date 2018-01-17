24d4d1278e7503210d2015ba4dca257290627128
Phil Maurer <philmau@gmail.com>
2018-01-17 08:25:42 +0000

    Replacing docker compose with repo package version

--- vagrant/cookbooks/docker/recipes/default.rb
+++ vagrant/cookbooks/docker/recipes/default.rb
@@ -1,13 +1,10 @@
 # Ensure docker is installed
-package "docker"
+package [
+	"docker",
+	"docker-compose"
+]
 
-# Ensure docker-compose is installed
-execute "Install docker-compose" do
-	command "curl -L https://github.com/docker/compose/releases/download/1.13.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose"
-	creates "/usr/local/bin/docker-compose"
-end
-
-# Start php-fpm service
+# Start docker service
 service 'docker' do
   action :start
 end
